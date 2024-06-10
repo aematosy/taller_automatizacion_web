67896c30b46588ca0625d696ff6383afac64f89a
Adrian Matos <amatos3@gmail.com>
2024-06-09 23:41:36 -0500

    update files

---
+++ src/test/java/com/taller/Constants/HomeConstants.java
@@ -0,0 +1,9 @@
+package com.taller.Constants;
+
+import org.openqa.selenium.By;
+
+public class HomeConstants {
+
+    public static final By REGISTER_OPTION = By.xpath("//a[@href='register.php']");
+}
+
